f9979769af71bfcaf3c20a907444e1292f0ae0da
shoushen <shoushen@yzyw-inc.com>
2019-07-08 18:12:20 +0800

    upgrade version

--- tsharding-spring-boot/src/main/java/com/lyh/tsharding/config/SpringDataSourceProcess.java
+++ tsharding-spring-boot/src/main/java/com/lyh/tsharding/config/SpringDataSourceProcess.java
@@ -4,24 +4,24 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 
 public class SpringDataSourceProcess {
-	private static final Logger LOGGER = LoggerFactory.getLogger(SpringDataSourceProcess.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(SpringDataSourceProcess.class);
 
-	public static void process() {
-		LOGGER.warn("Tsharding.SpringBoot.process start.............");
-		if (DataSourceBuilder.create().findType() != null) {
-			try {
-				Field nameField = DataSourceBuilder.class.getDeclaredField("DATA_SOURCE_TYPE_NAMES");
-				Field modifiersField = Field.class.getDeclaredField("modifiers");
-				modifiersField.setAccessible(true);
-				modifiersField.setInt(nameField, nameField.getModifiers() & ~Modifier.FINAL);
-				nameField.setAccessible(true);
-				nameField.set(null, new String[0]);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+  public static void process() {
+    LOGGER.warn("Tsharding.SpringBoot.process start.............");
+    if (DataSourceBuilder.findType(DataSourceBuilder.class.getClassLoader()) != null) {
+      try {
+        Field nameField = DataSourceBuilder.class.getDeclaredField("DATA_SOURCE_TYPE_NAMES");
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(nameField, nameField.getModifiers() & ~Modifier.FINAL);
+        nameField.setAccessible(true);
+        nameField.set(null, new String[0]);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
 }
